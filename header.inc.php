740b6c4f5fce5bbbb773d7516f51b6003b1a72b7
jpayet <jean-francois.payet@etudiant.univ-reims.fr>
2022-03-03 22:23:03 +0100

    page accueil

--- header.inc.php
+++ header.inc.php
@@ -1,7 +1,7 @@
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">GAME LIBRARY</a>
+            <a class="navbar-brand" href="#"><span id="brand">Game</span>Library</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
