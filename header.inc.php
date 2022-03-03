80f2f361430ee9f04c1b7068cbdcdef1187e6186
jpayet <jean-francois.payet@etudiant.univ-reims.fr>
2022-03-04 00:40:35 +0100

    form

--- header.inc.php
+++ header.inc.php
@@ -8,7 +8,7 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-item nav-link active" href="index.php">Accueil <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Catalogue</a>
+                    <a class="nav-item nav-link" href="listing.php">Catalogue</a>
                     <a class="nav-item nav-link" href="form_recherche.php">Recherche</a>
                     <a class="nav-item nav-link" href="#">Administration</a>
                 </div>
---
+++ listing.php
@@ -0,0 +1,9 @@
+<?php
+    require 'debut_html.inc.php';
+    require 'header.inc.php';
+?>
+
+<?php
+    require 'footer_html.inc.php';
+    require 'fin_html.inc.php';
+?>
