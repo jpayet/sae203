00eb36ee3f3b3549e54646673c88a2dd1413c772
jpayet <jean-francois.payet@etudiant.univ-reims.fr>
2022-03-21 10:03:06 +0100

    reponse recherche

---
+++ reponse_recherche.php
@@ -0,0 +1,27 @@
+<?php
+    require 'debut_html.inc.php';
+?>
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#"><span id="brand">Game</span>Library</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="index.php">Accueil <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="listing.php">Catalogue</a>
+                    <a class="nav-item nav-link" href="form_recherche.php">Recherche</a>
+                    <a class="nav-item nav-link" href="#">Administration</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    
+
+<?php
+    require 'footer_html.inc.php';
+    require 'fin_html.inc.php';
+?>
